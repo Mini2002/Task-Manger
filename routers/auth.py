5f4017bc69ab3835cf74acee33e78de73204fd8d
Mini Agarwal <miniagarwal2002Gmail.com>
2025-06-07 19:05:22 +0530

    add users table and user class to create new users

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 
-@router.get("/auth")
-async def user_auth():
-    return {'user': 'authentication'}
+class userRequest(BaseModel):
+    email: str
+    username : str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+
+@router.post("/auth")
+async def create_user(create_user: userRequest):
+    create_user_model = Users(
+        email= create_user.email,
+        username = create_user.username,
+        first_name= create_user.first_name,
+        last_name= create_user.last_name,
+        hashed_password= bcrypt_context.hash(create_user.password),
+        role= create_user.role,
+        is_active= True
+    )
+    return create_user_model
